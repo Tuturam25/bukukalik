cf2334f17f1aff3b5a0bf5ca01670b4d709d7e0c
tuturam <ahmadrama251005@gmail.com>
2023-10-13 16:43:43 +0700

    nambah foto

---
+++ buku.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Okt 2023 pada 09.12
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `buku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `kategori` varchar(250) NOT NULL,
+  `namabuku` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `gambar` longblob NOT NULL,
+  `penerbit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `kategori`, `namabuku`, `harga`, `stok`, `gambar`, `penerbit`) VALUES
+(5, 'lanciaw', '10 dosa besar msh', 100000, 1002, '', 4),
+(6, 'lanciaw', 'adsas', 12313, 212, '', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` int(11) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `kota` varchar(200) NOT NULL,
+  `telepon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `nama`, `alamat`, `kota`, `telepon`) VALUES
+(4, 'nopan', 'adksaodk', 'akdasok', 2147483647),
+(5, 'sda', 'sdadas', 'dasdasda', 31231);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  MODIFY `id_penerbit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -38,8 +38,20 @@ if(isset($_POST['konfirmasi'])) {
     $harga = $_POST['harga'];
     $stok = $_POST['stok'];
     $penerbit = $_POST['penerbit'];
+    $gambar = $_FILES['gambar']['tmp_name'];
+    $gambars = $_FILES['gambar']['name'];
     $id_buku = $_POST['id_buku'];
-    mysqli_query($conn, "update buku set kategori = '$kategori', namabuku = '$namabuku', harga = '$harga', stok = '$stok', penerbit = '$penerbit' where id_buku = '$id_buku'");
+    mysqli_query($conn, "update buku set kategori = '$kategori', namabuku = '$namabuku', harga = '$harga', stok = '$stok', penerbit = '$penerbit', gambar = '$gambars' where id_buku = '$id_buku'");
+    move_uploaded_file($gambar, "upload/$gambars");
+    header('refresh: 0');
+}
+if(isset($_POST['konfirmasii'])) {
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $kota = $_POST['kota'];
+    $telepon = $_POST['telepon'];
+    $id_penerbit = $_POST['id_penerbit'];
+    mysqli_query($conn, "update penerbit set nama = '$nama', alamat = '$alamat', kota = '$kota', telepon = '$telepon' where id_penerbit = '$id_penerbit'");
     header('refresh: 0');
 }
 
@@ -47,12 +59,13 @@ if(isset($_POST['konfirmasi'])) {
 
 <link rel="stylesheet" href="output.css">
 
-<div class="">
-    <form action="" method="get">
-        <input type="submit" name="home" value="home">
-        <input type="submit" name="admin" value="admin">
-        <input type="submit" name="pengadaan" value="pengadaan">
-    </form>
+<div class="bg-sky-800 flex gap-10 justify-between items-center text-white capitalize text-lg px-5">
+    <span class="p-5 font-mono text-3xl">BukuKalik</span>
+    <div class="flex p-5 gap-5">
+    <a href="index.php">home</a>
+    <a href="?admin">admin</a>
+    <a href="?pengadaan">pengadaan</a>
+    </div>
 </div>
 
 
@@ -65,101 +78,95 @@ pemula
 <?php endif ?>
 
 <?php if(isset($_GET['admin'])) { ?>
-
-    <table border="1">
-        <tr>
-            <th>kategori</th>
-            <th>nama buku</th>
-            <th>harga</th>
-            <th>stok</th>
-            <th>nama penerbit</th>
-            <th>aksi</th>
-        </tr>
-        <?php foreach(mysqli_query($conn, 'select * from penerbit') as $penerbit) { ?>
-        <tr>
-            <td>
+    <div class="p-5">
+        <table class="w-full capitalize text-center">
+            <tr>
+                <th>nama penerbit</th>
+                <th class="border-x-2 border-b-2 p-3">alamat</th>
+                <th class="border-x-2 border-b-2 p-3">kota</th>
+                <th class="border-x-2 border-b-2 p-3">telepon</th>
+                <th>aksi</th>
+            </tr>
+            <?php foreach(mysqli_query($conn, 'select * from penerbit') as $penerbit) { ?>
+            <tr>
                 <form action="" method="post">
-                <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
-                        <input type="hidden" name="id_penerbit" value="<?= $penerbit['id_penerbit'] ?>">
-                        <input type="text" name="nama" value="<?= $penerbit['nama'] ?>">
-                <?php } else { 
-                    echo $penerbit['nama'];
-                }?>
-            </td>
-            <td>
-                <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
-                    <input type="text" name="alamat" value="<?= $penerbit['alamat'] ?>">
-                <?php } else { 
-                    echo $penerbit['alamat'];
-                }?>    
-            </td>
-            <td>
-                <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
-                    <input type="number" name="kota" value="<?= $penerbit['kota'] ?>">
-                <?php } else { 
-                    echo $penerbit['kota'];
-                }?>
-            </td>
-            <td>
-                <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
-                    <input type="text" name="telepon" value="<?= $penerbit['telepon'] ?>">
-                <?php } else { 
-                    echo $penerbit['telepon'];
-                }?>
-            </td>
-            <td>
-                <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
-                    <select name="penerbit" id="">
-                    <option value="">pilih penerbit</option>
-                    <?php foreach(mysqli_query($conn, "select * from penerbit") as $terbit) { ?>
-                        <option <?php if($terbit['nama'] == $penerbit['nama']) echo 'selected' ?> value="<?= $terbit['id_penerbit'] ?>"><?= $terbit['nama'] ?></option>
-                    <?php } ?>
-                    </select>
+                <td class="border-y-2 border-r-2 p-3">
+                    <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
+                            <input type="hidden" name="id_penerbit" value="<?= $penerbit['id_penerbit'] ?>" >
+                            <input type="text" name="nama" value="<?= $penerbit['nama'] ?>" class="border-2 text-center">
                     <?php } else { 
                         echo $penerbit['nama'];
                     }?>
-            </td>
-            <td>    
-                    <?php if(!isset($_POST['ubah'])) : ?>
-                    <button name="ubah">ubah</button>
-                    <input type="hidden" name="id" value="<?= $penerbit['id_penerbit'] ?>">
-                    <?php elseif(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) : ?>
-                        <button name="konfirmasi">konfirmasi</button>
-                        <button name="batal">batal</button>
-                    <?php endif ?>
-                </form>
-            </td>
-            </tr>
-        <?php } ?>
-    </table>
+                </td>
+                <td class="border-y-2 border-r-2 p-3">
+                    <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
+                        <input type="text" name="alamat" value="<?= $penerbit['alamat'] ?>" class="border-2 text-center">
+                    <?php } else { 
+                        echo $penerbit['alamat'];
+                    }?>    
+                </td>
+                <td class="border-y-2 border-r-2 p-3">
+                    <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
+                        <input type="text" name="kota" value="<?= $penerbit['kota'] ?>" class="border-2 text-center">
+                    <?php } else { 
+                        echo $penerbit['kota'];
+                    }?>
+                </td>
+                <td class="border-y-2 border-r-2 p-3">
+                    <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) { ?>
+                        <input type="text" name="telepon" value="<?= $penerbit['telepon'] ?>" class="border-2 text-center">
+                    <?php } else { 
+                        echo $penerbit['telepon'];
+                    }?>
+                </td>
+                <td class="border-y-2">    
+                        <?php if(isset($_POST['ubah']) and $_POST['id'] == $penerbit['id_penerbit']) : ?>
+                        <button name="konfirmasii" class="bg-sky-500 py-1 px-2 rounded-md capitalize text-white hover:bg-sky-600 transition-colors duration-300">konfirmasi</button>
+                        <button name="batal" class="bg-red-800 py-1 px-2 rounded-md capitalize text-white hover:bg-sky-950 transition-colors duration-300">batal</button>
+                            <?php else : ?>
+                                <button name="ubah" class="bg-sky-800 py-1 px-2 rounded-md capitalize text-white hover:bg-sky-950 transition-colors duration-300">ubah</button>
+                                <input type="hidden" name="id" value="<?= $penerbit['id_penerbit'] ?>">
+                        <?php endif ?>
+                    </form>
+                </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
 
-    <form action="" method="post">
-    <label for="">nama penerbit</label>
-    <input type="text" name="penerbit">
-    <label for="">alamat</label>
-    <input type="text" name="alamat">
-    <label for="">kota</label>
-    <input type="text" name="kota">
-    <label for="">telepon</label>
-    <input type="number" name="telepon">
-    <input type="submit" value="tambah" name="tambah">
-</form>
+        <a href="#tambahadmin" class="rounded-full border-4 w-10 h-10 flex justify-center items-center p-10">&plus;</a>
+
+    <div class="hidden target:block" id="tambahadmin">
+        <a href="?admin" class="rounded-full border-4 w-5 h-5 flex justify-center items-center p-5">x</a>
+        <form action="" method="post">
+            <label for="">nama penerbit</label>
+            <input type="text" name="penerbit">
+            <label for="">alamat</label>
+            <input type="text" name="alamat">
+            <label for="">kota</label>
+            <input type="text" name="kota">
+            <label for="">telepon</label>
+            <input type="number" name="telepon">
+            <input type="submit" value="tambah" name="tambah">
+        </form>
+    </div>
 <?php } ?>
 
 <?php if(isset($_GET['pengadaan'])) { ?>
-    <table border="1">
+    <table class="w-full text-center">
         <tr>
             <th>kategori</th>
             <th>nama buku</th>
             <th>harga</th>
             <th>stok</th>
             <th>nama penerbit</th>
+            <th>gambar</th>
             <th>aksi</th>
         </tr>
         <?php foreach(mysqli_query($conn, 'select * from buku join penerbit on buku.penerbit = penerbit.id_penerbit') as $buku) { ?>
         <tr>
+            <form action="" method="post" enctype="multipart/form-data">
             <td>
-                <form action="" method="post">
                 <?php if(isset($_POST['ubah']) and $_POST['id'] == $buku['id_buku']) { ?>
                         <input type="hidden" name="id_buku" value="<?= $buku['id_buku'] ?>">
                         <input type="text" name="kategori" value="<?= $buku['kategori'] ?>">
@@ -199,6 +206,13 @@ pemula
                     <?php } else { 
                         echo $buku['nama'];
                     }?>
+            </td>
+            <td>
+                <?php if(isset($_POST['ubah']) and $_POST['id'] == $buku['id_buku']) { ?>
+                    <input type="file" name="gambar">
+                <?php } else { ?>
+                    <img src="upload/<?= $buku['gambar'] ?>" alt="" width="100">
+                <?php }?>
             </td>
             <td>    
                     <?php if(!isset($_POST['ubah'])) : ?>
